f989d50113bbfc3da8f1feb5769ebe3796a5f09f
Manikanta Polubothu <manikanta,polubothu9@gmail.com>
2023-02-22 09:22:13 +0530

    Function

--- 01-sample.sh
+++ 01-sample.sh
@@ -1,6 +1,11 @@
 xyz() {
   echo hello world from function
+  echo first argument - $1
+  echo second argument - $2
+  echo all arguments - $*
+  echo number of arguments - $#
 
 }
 
-xyz
+xyz 123 456 543
+
